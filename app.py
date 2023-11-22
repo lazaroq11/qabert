3172578a2208a8cd48cfa354f8fbe8d9b65abae5
lazaro-silva <lazaro.silva@hp.com>
2023-11-22 19:38:19 -0300

    change requirements

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from transformers import pipeline
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -35,4 +36,5 @@ def chat():
     return jsonify({'response': response})
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
